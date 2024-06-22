3d2d694d387041a9aef46b21b6c4c52281bea303
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-06-23 04:11:23 +0500

    get user

--- handler_user.go
+++ handler_user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/alphadev97/rss-aggregator/internal/auth"
 	"github.com/alphadev97/rss-aggregator/internal/database"
 	"github.com/google/uuid"
 )
@@ -37,6 +38,22 @@ func (apiCfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+
+	responsWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig)handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetApiKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByApiKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
+		return
+	}
 
 	responsWithJSON(w, 200, databaseUserToUser(user))
 }
